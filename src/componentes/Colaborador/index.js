076d070eca9df0ce7ba0960bb7abd702c5ad49ff
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-04-19 14:49:17 -0300

    change colors

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,8 +1,10 @@
 import './colaborador.css'
 import React from 'react'
+import { AiFillCloseCircle } from 'react-icons/ai'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
+        <AiFillCloseCircle size={25} className='deletar' onClick={aoDeletar}/>
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
