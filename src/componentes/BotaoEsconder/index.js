1f27d799c10b4a8c86054ffc48fcb254263ac302
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-04-25 12:33:46 -0300

    hide form button

--- src/componentes/BotaoEsconder/index.js
+++ src/componentes/BotaoEsconder/index.js
@@ -2,38 +2,13 @@ import { useState } from "react";
 import "./botao-esconder.css";
 import React from "react";
 
-// const BotaoEsconder = () => {
-//   const [show, setShow] = useState(false);
-//   return (
-//     <button
-//       className="botao-esconder"
-//       style={{ backgroundImage: 'url("/imagens/Botão add-01 1.png")' }}
-//     >
-//     </button>
-//   );
-// };
-
-// const BotaoEsconder = () => {
-//   const [show, setShow] = useState(true);
-//   return (
-//     <>
-//       <button onClick={() => setShow(!show)}>
-//         {show ? `Ocultar` : `Mostrar`}
-//       </button>
-//       <div className={show ? "show-element" : null}>
-//         {show && <h1>Exemplo</h1>}
-//       </div>
-//     </>
-//   );
-// }
-
 function BotaoEsconder(props) {
   console.log(props);
-  const [mostrar, setMostrar] = useState(true);
   return (
     <>
-      <button onClick={() => setMostrar(!mostrar)}>Clique aqui!</button>
-      {mostrar && "<Formulario />"}
+      <button onClick={props.onClick}> 
+        <img src="/imagens/Botão add-01 1.png"/>
+      </button>
     </>
   );
 }
